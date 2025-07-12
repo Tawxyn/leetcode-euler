434f8b13f155619ae9b122aface646cf9512707a
Mike Oleshchuk <144498628+Tawxyn@users.noreply.github.com>
2025-07-12 16:05:57 -0400

    Update problem1.cs

--- projecteuler/problem001/problem1.cs
+++ projecteuler/problem001/problem1.cs
@@ -1,1 +1,22 @@
-
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		int sum = 0;
+		
+		for (int i = 0; i < 1000; i++)
+		{
+			if (i % 3 == 0)
+			{
+				sum += i;
+			}
+			else if (i % 5 == 0)
+			{
+				sum += i;
+			}
+		}
+		Console.WriteLine(sum);	
+	}
+}
