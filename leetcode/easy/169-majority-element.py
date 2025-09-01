dc97151d4ab85a78995e517bf986e75b78f1a845
Mike Oleshchuk <144498628+Tawxyn@users.noreply.github.com>
2025-08-31 21:26:58 -0700

    Optimize majorityElement method with Boyer–Moore

--- leetcode/easy/169-majority-element.py
+++ leetcode/easy/169-majority-element.py
@@ -1,10 +1,21 @@
+#class Solution(object):
+#    def majorityElement(self, nums):
+#        count = {}
+#
+#        for i in range(len(nums)):
+#            if nums[i] not in count:
+#                count[nums[i]] = 0
+#            count[nums[i]] += 1
+#        
+#        return max(count, key=count.get)
+
+# Boyer–Moore Voting Algorithm, this is the optimal solution
 class Solution(object):
     def majorityElement(self, nums):
-        count = {}
+        res, count = 0, 0
 
-        for i in range(len(nums)):
-            if nums[i] not in count:
-                count[nums[i]] = 0
-            count[nums[i]] += 1
-        
-        return max(count, key=count.get)
+        for n in nums:
+            if count == 0:
+                res = n
+            count += (1 if n == res else -1)
+        return res
